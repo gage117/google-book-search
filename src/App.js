0cb4b5542195928f1891774a2b86aac56949b348
gage117 <57643501+gage117@users.noreply.github.com>
2020-02-14 11:40:55 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       
+    }
+  }
+
+
+  render() {
+    function handleSearchSubmit(searchString) {
+      console.log(searchString)
+    }
+
+    return (
+      <main className="App">
+        <header className="App-header">
+          <h1>Google Book Search</h1>
+        </header>
+          <form className='search-bar'>
+            <label>Search:
+              <input type='text' required></input>
+              <button type='submit' onClick={handleSearchSubmit}>Submit</button>
+            </label>
+          </form>
+          <section>
+            <label>Print Type:
+              <select>
+                <option>All</option>
+                <option>Printed</option>
+                <option>E-Book</option>
+              </select>
+            </label>
+            <label>Book Type:
+              <select>
+                <option>No Filter</option>
+                <option>Paid</option>
+                <option>Free</option>
+              </select>
+            </label>
+          </section>
+          <ul className='results-list'>
+
+          </ul>
+      </main>
+    );
+  }
 }
+
 
 export default App;
